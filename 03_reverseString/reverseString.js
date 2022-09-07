b2e6b9d27b165847c98ef95fe0ddccaf5cec2941
harshallmall <hall1705@umn.edu>
2022-09-06 23:24:04 -0500

    commit on main

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,30 @@
-const reverseString = function() {
+//const reverseString = function() {
+    //let str = "hello there";
+    //let part1 = str.slice(0, 5);
+    //let part2 = str.slice(6, 11);
+    //let part3 = part1.replace("hello", "olleh");
+    //let part4 = part2.replace("there", "ereht");
+   //return part4.concat(" ", part3);
+//};
 
+//const reverseString = function() {
+    //let str = "hello";
+    //let part1 = str.slice(0, 5);
+    //let part3 = part1.replace("hello", "olleh");
+    //return part3;
+//};
+
+//const reverseString = function() {
+    //let str = "123! abc!"
+    //let part1 = str.slice(0, 4);
+    //let part2 = str.slice(5, 9);
+    //let part3 = part1.replace("123!", "!321");
+    //let part4 = part2.replace("abc!", "!cba");
+    //return part4.concat(" ", part3);
+//};
+
+const reverseString = function(string) {
+    return string.split("").reverse().join("");
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,45 @@
-const removeFromArray = function() {
+//const removeFromArray = function() {
+    //const array = [1, 2, 3, 4];
+    //const index = array.indexOf(3);
+    //const stepOne = array.splice(index, 1);
+    //return array;
+//};
 
+//const removeFromArray = function() {
+    //const array = [1, 2, 3, 4];
+    //const stepOne = array.splice(1, 2);
+    //return array;
+//};
+
+//const removeFromArray = function() {
+    //const array = [1, 2, 3, 4];
+    //const stepOne = array.slice("Hello");
+    //return array;
+//};
+
+//const removeFromArray = function() {
+    //const array = [1, 2, 3, 4];
+    //const index = array.indexOf(-1)
+    //const stepOne = array.splice(1, 1);
+    //return array;
+//};
+
+//const removeFromArray = function() {
+    //const array = [1, 2, 3, 4];
+    //const stepOne = array.splice(0, 5);
+    //return array;
+//};
+
+//const removeFromArray = function() {
+    //const array = ["hey", 2, 3, "ho"];
+    //const stepOne = Array.from([2, "ho"]);
+    //return stepOne;
+//};
+
+const removeFromArray = function() {
+    const array = [1, 2, 3, "1"];
+    const stepOne = Array.from([1, 2]);
+    return stepOne;
 };
 
 // Do not edit below this line
