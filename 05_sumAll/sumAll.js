13b91a802910c09021d6ed50f53afed343d460d8
harshallmall <hall1705@umn.edu>
2022-09-13 20:59:21 -0500

    commit on main

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,10 @@
-const sumAll = function() {
-
-};
-
+function sumAll() {
+  let sum = 0;
+  const num = 4
+  for (let i = 1; i <= num; i++) {
+      sum += i;
+  }
+    return sum;
+ };
 // Do not edit below this line
 module.exports = sumAll;
